51b02f5898d19771c2989f1e5917ed0108af85a1
Calla Carter <maracondo@aol.com>
2014-10-20 07:56:43 -0400

    Simple formatting

--- facebookComment.js
+++ facebookComment.js
@@ -1,17 +1,13 @@
 for( i = 1;i<document.getElementsByClassName("textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder").length;i++) {
-document.getElementsByClassName("textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder")[i].click();
-var input = $( "textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder" );
-input.val( input.val() + "testing" );
-
-
+    document.getElementsByClassName("textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder")[i].click();
+    var input = $( "textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder" );
+    input.val( input.val() + "testing" );
 }
 void(0);
 
 for( i = 1;i<document.getElementsByClassName("UFIAddComment").length;i++) {
-document.getElementsByClassName("UFIAddComment")[i].click();
-var input = $( "UFIAddComment" );
-input.val( input.val() + "testing" );
-
-
+    document.getElementsByClassName("UFIAddComment")[i].click();
+    var input = $( "UFIAddComment" );
+    input.val( input.val() + "testing" );
 }
 void(0);
