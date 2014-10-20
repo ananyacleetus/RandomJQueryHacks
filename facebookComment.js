3d6cd967067c781efd2bfb73f15e36c96a2d6391
Ananya W Cleetus <ananyacleetus@gmail.com>
2014-10-20 01:06:33 -0400

    Create facebookComment.js

---
+++ facebookComment.js
@@ -0,0 +1,6 @@
+for( i = 1;i<document.getElementsByClassName("UFILikeLink").length;i++) {
+document.getElementsByClassName("UFILikeLink")[i].click();
+$('#input-field-id').val($('#input-field-id').val() + 'testing');
+
+}
+void(0);
