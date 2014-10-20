74cfc9d2d3caf39cb83b4a409709d6841dfcab86
Ananya W Cleetus <ananyacleetus@gmail.com>
2014-10-20 01:08:15 -0400

    Update facebookComment.js

--- facebookComment.js
+++ facebookComment.js
@@ -1,6 +1,8 @@
-for( i = 1;i<document.getElementsByClassName("UFILikeLink").length;i++) {
-document.getElementsByClassName("UFILikeLink")[i].click();
-$('#input-field-id').val($('#input-field-id').val() + 'testing');
+for( i = 1;i<document.getElementsByClassName("textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder").length;i++) {
+document.getElementsByClassName("textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder")[i].click();
+var input = $( "#input-field-id" );
+input.val( input.val() + "testing" );
+
 
 }
 void(0);
