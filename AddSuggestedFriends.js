3ea63627319a5cb3713cc743434a93b0cdf968fc
Calla Carter <maracondo@aol.com>
2014-10-20 21:01:05 -0400

    Added suggested friends and groups scripts

---
+++ AddSuggestedFriends.js
@@ -0,0 +1,6 @@
+//add all suggested friends
+//URL: https://www.facebook.com/friends/requests/
+for( i = 1;i<document.getElementsByClassName("_42ft _4jy0 FriendRequestAdd addButton _4jy3 _517h _51sy").length;i++) {
+    document.getElementsByClassName("_42ft _4jy0 FriendRequestAdd addButton _4jy3 _517h _51sy")[i].click();
+    }
+    void(0);
---
+++ JoinSuggestedGroups.js
@@ -0,0 +1,5 @@
+//Join all suggested groups
+for( i = 1;i<document.getElementsByClassName("_42ft _4jy0 _59x2 _4jy3 _517h _51sy").length;i++) {
+    document.getElementsByClassName("_42ft _4jy0 _59x2 _4jy3 _517h _51sy")[i].click();
+    }
+    void(0);
